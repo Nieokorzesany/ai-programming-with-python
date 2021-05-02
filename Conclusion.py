a95e06177078e9f0afeec241de8afa906bf4461c
Pancu <pancu1222@gmail.com>
2021-05-02 20:59:21 +0200

    commit to repository

---
+++ Conclusion.py
@@ -0,0 +1,38 @@
+
+print("Statistics for ALEXNET: \n \
+N Images: 40  N Dog Images: 30  N NotDog Images: 10\n \
+Pct Corr dog: 100.0 Pct Corr NOTdog: 100.0  Pct Corr Breed:  80.0\n\
+Results for model: ALEXNET\n\
+Number of images: 40\n\
+Number of Dog images: 30\n\
+Number of Not Dog images: 10\n\
+pct_match :  75.0 % \n\
+pct_correct_dogs :  100.0 % \n\
+pct_correct_breed :  80.0 % \n\
+pct_correct_notdogs :  100.0 %\n")
+
+print('STatistics for VGG\n\
+N Images: 40  N Dog Images: 30  N NotDog Images: 10 \n\
+Pct Corr dog: 100.0 Pct Corr NOTdog: 100.0  Pct Corr Breed:  93.3\n\
+Results for model: VGG\n\
+Number of images: 40\n\
+Number of Dog images: 30\n\
+Number of Not Dog images: 10\n\
+pct_match :  87.5 %\n\
+pct_correct_dogs :  100.0 %\n\
+pct_correct_breed :  93.3 %\n\
+pct_correct_notdogs :  100.0 % \n')
+
+print('STatistics for RESNET\n\
+      N Images: 40  N Dog Images: 30  N NotDog Images: 10 \n\
+Pct Corr dog: 100.0 Pct Corr NOTdog:  90.0  Pct Corr Breed:  90.0\n\
+Results for model: RESNET\n\
+Number of images: 40\n\
+Number of Dog images: 30\n\
+Number of Not Dog images: 10\n\
+pct_match :  82.5 %\n\
+pct_correct_dogs :  100.0 %\n\
+pct_correct_breed :  90.0 %\n\
+pct_correct_notdogs :  90.0 %\n')
+      
+print('Based on the results the best architecture model is VGG')
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # ai-programming-with-python
+
+
+This repository contains REVISED code and associated files for the AI Programming with Python Nanodegree program. This repository consists of a number of tutorial notebooks for various coding exercises and programming labs that will be used to supplement the lessons of the course.
+
+
+### pip
+
+To install these dependencies with pip, you can issue `pip3 install -r requirements.txt`.
